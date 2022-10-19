c33a2614a4fd5b3f2cb00ec728842f8f90aadd4d
Jakub Obstarczyk <jakub.obstarczyk@enjin.io>
2022-10-19 14:42:29 +0200

    add MainPageConst.cs

---
+++ CouponFollow.Web.Tests/PageObjects/MainPageConst.cs
@@ -0,0 +1,15 @@
+namespace CouponFollow.Web.Tests.PageObjects;
+
+public static class MainPageConst
+{
+    public static class Selectors
+    {
+        public const string TodaysTrendingCoupons = ".trending-deals.cont > article";
+        public const string TopDealsSwiper = ".big-cont.top-deals-swiper";
+    }
+
+    public static class Text
+    {
+        public const string Title = "Coupon Codes in Real-Time - CouponFollow";
+    }
+}
