b5730e56cadc8d3038152f1481694a0ecfd2a87c
Jakub Obstarczyk <jakub.obstarczyk@enjin.io>
2022-10-19 19:41:50 +0200

    added MainPageConst.cs to store selectors and strings for validation

--- CouponFollow.Web.Tests/PageObjects/MainPageConst.cs
+++ CouponFollow.Web.Tests/PageObjects/MainPageConst.cs
@@ -4,6 +4,8 @@ public static class MainPageConst
 {
     public static class Selectors
     {
+        public const string StaffPicks = "div.staff-picks [data-domain]";
+        public const string TodaysTopCoupons = ".swiper-wrapper div:not(.swiper-slide-duplicate)";
         public const string TodaysTrendingCoupons = ".trending-deals.cont > article";
         public const string TopDealsSwiper = ".big-cont.top-deals-swiper";
     }
