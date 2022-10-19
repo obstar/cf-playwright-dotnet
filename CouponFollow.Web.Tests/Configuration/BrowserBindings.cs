047e1d37c369b616710a3866f8343eb7ad1b7449
Jakub Obstarczyk <jakub.obstarczyk@enjin.io>
2022-10-19 14:41:06 +0200

    add BrowserBindings.cs

---
+++ CouponFollow.Web.Tests/Configuration/BrowserBindings.cs
@@ -0,0 +1,61 @@
+﻿using BoDi;
+using CouponFollow.Web.Tests.PageObjects;
+using Microsoft.Playwright;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using TechTalk.SpecFlow;
+
+namespace CouponFollow.Web.Tests.Configuration;
+
+[Binding]
+public class BrowserBindings
+{
+    [BeforeScenario]
+    public async Task BeforeScenario(IObjectContainer specFlowContainer,
+                                     ScenarioContext scenarioContext)
+    {
+        Console.WriteLine($"-> [BeforeScenario] {scenarioContext.ScenarioInfo.Title}");
+
+        var playwright = await Playwright.CreateAsync();
+        var browserType = playwright["Chromium"];
+        var browser = await browserType.LaunchAsync(new BrowserTypeLaunchOptions
+                                                    {
+                                                        Headless = false,
+                                                        SlowMo = 500
+                                                    });
+
+        var mainPage = new MainPage(browser);
+        specFlowContainer.RegisterInstanceAs(playwright);
+        specFlowContainer.RegisterInstanceAs(browser);
+        specFlowContainer.RegisterInstanceAs(mainPage);
+    }
+
+    [AfterScenario(Order = 10)]
+    public async Task AfterEachScenario(IObjectContainer specFlowContainer,
+                                        ScenarioContext scenarioContext)
+    {
+        var nUnitStatus = TestContext.CurrentContext.Result.Outcome.Status;
+        var failed = nUnitStatus is not TestStatus.Passed and not TestStatus.Skipped;
+        Console.WriteLine($"-> [AfterScenario] {scenarioContext.ScenarioInfo.Title}");
+
+        var browser = specFlowContainer.Resolve<IBrowser>();
+        await browser.CloseAsync();
+        var playwright = specFlowContainer.Resolve<IPlaywright>();
+        playwright.Dispose();
+
+        try
+        {
+            if (!failed) return;
+            var error = scenarioContext.TestError;
+            Console.WriteLine("-> [AfterScenario] test has failed!");
+            Console.WriteLine($"-> [AfterScenario] An error occurred: {(error != null ? error.Message : "unknown")}");
+            Console.WriteLine(
+                              $"-> [AfterScenario] It was of type: {(error != null ? error.GetType().Name : "unknown")}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("-> Could not mark session as failed");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
